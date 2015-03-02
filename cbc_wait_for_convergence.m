7d9d4e37012fc87bdab58d4d650422b97cdc4cde
David AW Barton <david.barton@bristol.ac.uk>
2015-03-02 13:29:32 +0000

    Altered convergence criteria in wait_for_convergence

--- cbc_wait_for_convergence.m
+++ cbc_wait_for_convergence.m
@@ -16,14 +16,22 @@ pause(exp.opt.wait_time);
 % Get the handle to the RTC
 rtc = exp.rtc;
 
-% Wait until the normalised variance is sufficiently small
+% Wait until the absolute variance or the normalised variance is
+% sufficiently small (either being true is sufficient)
 iter = 1;
-while (iter < exp.opt.max_waits) && any(rtc.par.x_coeffs_var/norm(rtc.par.x_coeffs_ave) > exp.opt.x_coeffs_var_tol)
+x_coeffs_ave_norm = norm(rtc.par.x_coeffs_ave(rtc.fourier.idx_AC)); % Don't use the offset for normalisation
+x_coeffs_var = rtc.par.x_coeffs_var;
+while (iter < exp.opt.max_waits) ...
+        && any(x_coeffs_var > exp.opt.x_coeffs_var_tol_abs) ...
+        && any(x_coeffs_var/x_coeffs_ave_norm > exp.opt.x_coeffs_var_tol_rel)
     pause(exp.opt.wait_time);
     iter = iter + 1;
+    x_coeffs_ave_norm = norm(rtc.par.x_coeffs_ave(rtc.fourier.idx_AC)); % Don't use the offset for normalisation
+    x_coeffs_var = rtc.par.x_coeffs_var;
 end
 
-if any(rtc.par.x_coeffs_var/norm(rtc.par.x_coeffs_ave) > exp.opt.x_coeffs_var_tol)
+if any(x_coeffs_var > exp.opt.x_coeffs_var_tol_abs) ...
+        && any(x_coeffs_var/x_coeffs_ave_norm > exp.opt.x_coeffs_var_tol_rel)
     success = false;
 else
     success = true;
